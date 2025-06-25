3019698d58cd685e2cb52b0ef98d48253e90efa2
omid-farahani <omid.farahani.dev@gmail.com>
2025-06-25 14:29:56 -0700

    fixed js and add favicon

--- js/App.js
+++ js/App.js
@@ -3,15 +3,15 @@ const navCloseBtn = document.querySelector(".nav-close-btn");
 const nav = document.querySelector(".nav");
 const overlay = document.querySelector(".overlay");
 
-function closeNav() {
-    nav.classList.remove("right-0");
-    nav.classList.add("-right-96");
-    overlay.classList.remove("overlay--visible");
-}
 navOpenBtn.addEventListener("click",() => {
-    nav.classList.remove("-right-64");
+    nav.classList.remove("-right-[1000px]");
     nav.classList.add("right-0");
     overlay.classList.add("overlay--visible");
     overlay.addEventListener("click", closeNav)
 })
+function closeNav() {
+    nav.classList.remove("right-0");
+    nav.classList.add("-right-[1000px]");
+    overlay.classList.remove("overlay--visible");
+}
 navCloseBtn.addEventListener("click", closeNav);
